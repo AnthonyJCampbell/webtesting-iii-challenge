658228a6426cab0e70d48b697dfd9b52fc417653
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-04-10 09:31:35 +0200

    Added tests for dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,4 +1,19 @@
 // Test away
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 
+import Dashboard from './Dashboard';
 
-//- shows the controls and display
+describe('Dashboard component',() => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Dashboard />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />)
+    expect(tree.toJSON()).toMatchSnapshot();
+  })
+})
